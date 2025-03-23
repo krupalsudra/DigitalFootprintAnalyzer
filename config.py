2bff9a9ef091197690db92805967ed1053e75ed6
krupalsudra <sudrakrupal02@gmail.com>
2025-03-24 03:29:52 +0530

    Add files via upload
    commit

---
+++ config.py
@@ -0,0 +1,15 @@
+# ✅ Telegram Bot Configuration
+TELEGRAM_BOT_TOKEN = 
+"7778478472:AAG3vqv-CSmiarFBgDDQjdJtgGQlqrZ8oFo"
+
+# Replace with your bot token
+TELEGRAM_CHAT_ID = "6728315195"  # Replace with your actual chat ID
+
+# ✅ API Keys
+LEAKCHECK_API_KEY = "1aa966cec1397b3b48af57b9113cd1a43844e75f"
+
+GOOGLE_SAFE_BROWSING_API_KEY = "AIzaSyDMb0G6Oc-msfdigMLBI76PE_oAb-Mbk0M"
+
+MAILBOXLAYER_API_KEY = "2aa3ba418dc96c190452272aecaf7b75"
+
+
